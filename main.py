52eec73dc096bd6f061e2b303ff21ec479a97bb9
Ryan Rideout <ryancrideout@gmail.com>
2023-02-10 18:19:23 -0700

    Saving my work.

--- main.py
+++ main.py
@@ -56,11 +56,11 @@ class StateManager:
 
     state_index = 0
     state_list = []
+    current_state = None
 
     def __init__(self):
         # Need to initialize states, and then put them into the state list.
         self.initialize_states()
-        print("Shoryuken!")
 
     def initialize_states(self):
         state_0 = State_0()
@@ -69,10 +69,41 @@ class StateManager:
 
         self.state_list = [state_0, state_1, state_2]
 
-    def process_input(string: str):
-        print("Shoryuken!")
+        # Our initial state will always be state 0.
+        self.current_state = state_0
+
+    def process_input(self, binary_string: str):
+        """
+        Take the binary string and process it. This will also
+        give us our current state.
+        """
+        for binary_integer in binary_string:
+            input = int(binary_integer)
+            # Note: 0's are "Falsy" and 1's are Truthy.
+            if input:
+                # 1 as an input
+                self.state_index += self.state_index[self.state_index].one_input
+            else:
+                # 0 as an input
+                self.state_index += self.state_index[self.state_index].zero_input
+
+        self.set_current_state(self.state_list[self.state_index])
+
+    def set_current_state(self, state):
+        """
+        TODO: Abstract the state further so we can do type checking.
+
+        Method to assign the state. While we could have just assigned current_state
+        directly in the "process_input" method, I like having this as a separate
+        method as it allows for more freedom, and is useful for debugging.
+        """
+        self.current_state = state
 
 
+# Idea - make modulo_three a method for the state_manager (Hmm...)
+# Or make it so we don't have to initialize the classes every time.
+# Solution - initialize StateManager on start up, then trap the
+# user in a while loop. How devious.
 def modulo_three(string: str) -> float:
     """
     NOTE: Returns float for anticpation of decimal values.
@@ -81,12 +112,12 @@ def modulo_three(string: str) -> float:
     it and returns the answer, as a number.
     """
     state_manager = StateManager()
-    print("Shoryuken!")
+    state_manager.process_input(string)
 
 
 def execute():
-    modulo_three()
-    print("Shroyuken!")
+    modulo_three("1101")
+    print("Done!")
 
 
 execute()
